784c1bc935e00c1c9cf3530ae3c1ebf9a84e51f4
DrunkLeen <reza.df.x@gmail.com>
2024-03-17 10:22:50 +0100

    requirements file added

--- message.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-with open("word_list.txt", "r", encoding="utf-8") as file:
-    word_list = file.readlines()
-    random_word = random.choice(word_list).strip()
-    print("Random word:", random_word)
-
-print(random_word)
